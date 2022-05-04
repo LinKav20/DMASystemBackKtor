d456cf0309f8289d241f42bd1c4984493496774a
LinKa <maltseva-as@mail.ru>
2022-05-04 14:40:16 +0300

    email filling changed to html

--- dma-back/src/main/kotlin/linKav20/github/com/entities/sender/SenderController.kt
+++ dma-back/src/main/kotlin/linKav20/github/com/entities/sender/SenderController.kt
@@ -2,19 +2,36 @@ package linKav20.github.com.entities.sender
 
 
 import org.apache.commons.mail.DefaultAuthenticator
-import org.apache.commons.mail.SimpleEmail
+import org.apache.commons.mail.HtmlEmail
 
-val link = "http://localhost:8000/pass"
+val link = "http://localhost:8000/test"
+val message = "\"<!DOCTYPE html>" +
+        "<html>" +
+        "<head>" +
+        "<meta charset=utf-8>" +
+        "</head>" +
+        "<body>" +
+        "<h1>Оценка цифровой зрелости</h1>" +
+        "<p>Пройдите тест, оценив компанию, " +
+        "в которой вы работаете, по " +
+        "<a href=\\\"$link\\\"> " +
+        "ссылке </a>" +
+        "</p>" +
+        "</body" +
+        "></html>\""
+
 
 fun sendMessage(passings: List<String>) {
-    val email = SimpleEmail()
+    val email = HtmlEmail()
     email.hostName = "smtp.mail.ru"
     email.setSmtpPort(465)
     email.setAuthenticator(DefaultAuthenticator("dmasystem@mail.ru", "akRu5AhEDtpD0bxMM1Xe"))
     email.isSSLOnConnect = true
     email.setFrom("dmasystem@mail.ru")
     email.subject = "DMA: Оценка цифровой зрелости"
-    email.setMsg("Пройдите тест, оценив компанию, в которой вы работаете, по ссылке $link")
+    email.setCharset("UTF-8")
+    email.setHtmlMsg(message)
+    email.setTextMsg("Пройдите тест, оценив компанию, в которой вы работаете, по ссылке $link")
 
     for (passing in passings) {
         email.addTo(passing)
