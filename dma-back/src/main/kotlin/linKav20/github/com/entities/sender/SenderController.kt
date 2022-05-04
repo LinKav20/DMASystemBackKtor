c8c43d428298ab3bf98730a1e10d6e7257836ea4
LinKa <maltseva-as@mail.ru>
2022-05-04 13:53:18 +0300

    email filling changed

--- dma-back/src/main/kotlin/linKav20/github/com/entities/sender/SenderController.kt
+++ dma-back/src/main/kotlin/linKav20/github/com/entities/sender/SenderController.kt
@@ -4,6 +4,8 @@ package linKav20.github.com.entities.sender
 import org.apache.commons.mail.DefaultAuthenticator
 import org.apache.commons.mail.SimpleEmail
 
+val link = "http://localhost:8000/pass"
+
 fun sendMessage(passings: List<String>) {
     val email = SimpleEmail()
     email.hostName = "smtp.mail.ru"
@@ -11,8 +13,8 @@ fun sendMessage(passings: List<String>) {
     email.setAuthenticator(DefaultAuthenticator("dmasystem@mail.ru", "akRu5AhEDtpD0bxMM1Xe"))
     email.isSSLOnConnect = true
     email.setFrom("dmasystem@mail.ru")
-    email.subject = "Hello there"
-    email.setMsg("Be happy, it works!")
+    email.subject = "DMA: Оценка цифровой зрелости"
+    email.setMsg("Пройдите тест, оценив компанию, в которой вы работаете, по ссылке $link")
 
     for (passing in passings) {
         email.addTo(passing)
